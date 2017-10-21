00e3bff71db7ab0dd1ab4910b17646e21335a0a6
yoinkbird <YoinkBird2010@gmail.com>
2017-10-21 18:41:12 -0500

    refactor: hard-coded working example for serving up local files

--- server.py
+++ server.py
@@ -3,6 +3,39 @@ import SocketServer
 import json
 import cgi
 
+# hard-coded globals
+resource_dir = "res"
+quiet = 1
+
+
+# dump json to file for consumption by whatever else needs it
+def retrieve_json_file(filename="gps_scored_route.json"):
+    if ( quiet != 1):
+        print("# save to file")
+    # tmp:
+    filepath=("%s/%s" % (resource_dir, filename))
+    if ( quiet != 1):
+        print("mock-response sending to : " + filepath)
+    # with open(filepath, 'w') as outfile:
+    #    json.dump(response_json, outfile)
+
+    # verify
+    loadedjson = str()
+    with open(filepath, 'r') as infile:
+       loadedjson = json.load(infile)
+
+    loadedroute = json.loads(loadedjson)
+
+    return_value = -1
+    # verify
+    # if( response_json == loadedjson ):
+    #     print("json string resurrected successfully")
+    #     return_value = 1
+    # compare the dict if possible?
+
+    # return json string
+    return loadedjson
+
 class Server(BaseHTTPRequestHandler):
     def _set_headers(self):
         self.send_response(200)
@@ -15,7 +48,13 @@ class Server(BaseHTTPRequestHandler):
     # GET sends back a Hello world message
     def do_GET(self):
         self._set_headers()
-        self.wfile.write(json.dumps({'hello': 'world', 'received': 'ok'}))
+            #{'hello': 'world', 'received': 'ok'}
+        self.wfile.write(json.dumps(
+            retrieve_json_file()
+            ))
+        if ( quiet != 1):
+            print("you HAD one job - return the json! maybe you did? IDK")
+            print("-------------------------")
         
     # POST echoes the message adding a JSON field
     def do_POST(self):
@@ -30,24 +69,36 @@ class Server(BaseHTTPRequestHandler):
         # read the message and convert it into a python dictionary
         length = int(self.headers.getheader('content-length'))
         message = json.loads(self.rfile.read(length))
-        
-        # add a property to the object, just to mess with data
-        message['received'] = 'ok'
+
+# note sure if this could conflict with the map json        
+#        # add a property to the object, just to mess with data
+#        message['received'] = 'ok'
         
         # send the message back
         self._set_headers()
         self.wfile.write(json.dumps(message))
+
         
 def run(server_class=HTTPServer, handler_class=Server, port=8008):
     server_address = ('', port)
     httpd = server_class(server_address, handler_class)
     
-    print(('Starting httpd on port %d...' % port))
+    if ( quiet != 1):
+        print(('Starting httpd on port %d...' % port))
     httpd.serve_forever()
     
 if __name__ == "__main__":
     from sys import argv
     
+    # test
+    import pprint
+    if ( quiet != 1):
+        print("-------------------------")
+        print("you have one job - return this string!")
+        pprint.pprint(
+                retrieve_json_file()
+                )
+        print("-------------------------")
     if len(argv) == 2:
         run(port=int(argv[1]))
     else:
